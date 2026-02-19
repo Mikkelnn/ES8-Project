d7850bf6d6ec0023581eedbf7efbf567adc77845
Karl Andersen <brandt123kba@gmail.com>
2026-02-19 15:24:04 +0100

    Added Global timer and logger

--- simulator/src/custom_types.py
+++ simulator/src/custom_types.py
@@ -1,6 +1,25 @@
 from pydantic import BaseModel, Field
 from typing import List, Any
+from enum import Enum
 
+# Define allowed severities
+class Severity(str, Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+# Define allowed areas
+class Area(str, Enum):
+    SIMULATOR = "SIMULATOR"
+    NODE = "NODE"
+    GATEWAY = "GATEWAY"
+    EVENT = "EVENT"
+    BATTERY = "BATTERY"
+    CLOCK = "CLOCK"
+    TRANCEIVER = "TRANCEIVER"
+    OTHER = "OTHER"
 
 class EventNet(BaseModel):
     node_id: int
--- simulator/src/simulator/global_time.py
+++ simulator/src/simulator/global_time.py
@@ -1,6 +1,21 @@
-def main():
-    pass
 
+class time_global:
+    _instance = None
 
-if __name__ == "__main__":
-    main()
+    def __new__(cls): #Singlton so that no duplicates are possible, ie multiple time tracks
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._time = 0
+        return cls._instance
+
+    def get_time(self) -> int:
+        return self._time
+
+    def set_time(self, value: int):
+        self._time = int(value)
+
+    def increment_time(self, value: int = 1):
+        self._time += int(value)
+    
+    def decrement_time(self, value: int = 1):
+        self._time -= int(value)
