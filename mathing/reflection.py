abd1d30e88763290278f8c426b609fcfb016fe40
MhawnSendes <jtheil22@student.aau.dk>
2026-02-20 09:16:57 +0100

    skeleton for wave propogation is now done

--- mathing/reflection.py
+++ mathing/reflection.py
@@ -10,17 +10,32 @@ def refCoef(eta):
     return (eta[-1] - eta[-2])/(eta[-1] + eta[-2])
 
 def lossAngle(sigma, epsilon):
-    return np.arctan(sigma/(2*np.pi*conts.epsilon_0*epsilon))
+    return sigma/(2*np.pi*f*conts.epsilon_0*epsilon)
 
 def complexImpedance (epsilon, lossAngle):
-    return 1/(np.sqrt(epsilon*(1-1j*np.tan(lossAngle))))
+    return 1/(np.sqrt(epsilon*(1-1j*lossAngle)))
 
 def gammaCalc(ff):
     return 1j*2*np.pi*f/(conts.c*ff)
 
+def refCoefLen(KL, gamma, length):
+    return KL*np.exp(-2*gamma*length)
+
+def inputImpedance(eta, Kminus):
+    return eta*(1-Kminus)/(1+Kminus)
+
+def EplusNext(KL, KminusL, Eplus):
+    Etot1 = Eplus*(1+KL)
+    print(f"Total E-field at boundary: {Etot1}")
+    return Etot1/(1+KminusL)
+
+def firstMediumLoss(gamma, Length):
+    return E_0*np.exp(-gamma*Length)
+
 def dryDirt():
     epsilon_r = [2.5, 3]
-    conductivities = [1e-7,1e-4]
+    conductivities = [1e-7,1e-4] #in S/m
+    lengths = [0.7, 0.05] #in meters
     lossAngles = []
     for i in range(len(epsilon_r)):
         lossAngles.append(lossAngle(conductivities[i],epsilon_r[i]))
@@ -28,7 +43,7 @@ def dryDirt():
     ff = []
     for i in range(len(lossAngles)):
         ff.append(complexImpedance(epsilon_r[i],lossAngles[i]))
-        print(f"Loss angle: {lossAngles[i]}, intrinsic impedance angle: {np.angle(ff[i])*2}")
+        print(f"Loss angle: {np.arctan(lossAngles[i])}, intrinsic impedance angle: {np.angle(ff[i])*2}")
     
     gammas = []
     for i in range(len(ff)):
@@ -37,6 +52,16 @@ def dryDirt():
 
     eta = [eta_0*ff[0], eta_0*ff[1], eta_0]
     KL1 = refCoef(eta)
+    KminusL = refCoefLen(KL1, gammas[1], lengths[1])
+    eta2in = inputImpedance(eta[1], KminusL)
+    KL2 = refCoef([eta[0],eta2in])
+    print(f"Reflection coeffecient at first boundary: {KL2}")
+
+    EPlus1 = firstMediumLoss(gammas[0], lengths[0])
+    EPlus2 = EplusNext(KL2, KminusL, EPlus1)
+    ETotAir = np.abs(EPlus2*(1+KL1))
+    print(f"E+ at first boundary: {EPlus1}, E+ at second boundary: {EPlus2}, total E-field in the air: {ETotAir}")
+
 
 
 
