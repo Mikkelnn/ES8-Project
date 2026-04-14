74e6142d6784cb76dfb5da9cc5f660a32cf9231c
MhawnSendes <jtheil22@student.aau.dk>
2026-04-14 13:58:04 +0200

    We have now implemented temperature clock drift

--- mathing/clockSync/basicARClock.py
+++ mathing/clockSync/basicARClock.py
@@ -8,13 +8,22 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+np.random.seed(42069)
+
 t0 = 900
-c = np.array([0.92705, 0.4163, 0.07483, -0.387, -0.03118]) #AR-model constants from page 32
+c_std = np.array([0.92705, 0.4163, 0.07483, -0.387, -0.03118]) #AR-model constants from page 32
+c_temp = np.array([0.4397, 0.3106, 0.1874])
 init = np.array([0, 3.95e-5, 3.95e-5, 3.95e-5, 3.95e-5, 3.95e-5]) #initial conditions for the AR-model. alpha[0] is the variance for the clock skew see page 33
 init = np.transpose(init)
+init_Temp = np.array([0, 1.7e-3, 1.7e-3, 1.7e-3])
+init_Temp = np.transpose(init_Temp)
 noiseVar = 3.915e-15
-simLength = 365  #days
-np.random.seed(42069)
+noiseVarTemp = 6.9e-10
+
+simLength = 365 #days
+samplesDay = 24*3600/t0
+k_Temp = 5.559e-6
+
 
 
 def plotData(data):
@@ -96,7 +105,7 @@ def plot_multiple_realizations(num_realizations=10):
 
 def ARModelSimple():
     A = np.array([[1, t0, 0, 0, 0, 0],
-         [0, c[0], c[1], c[2], c[3], c[4]],
+         [0, c_std[0], c_std[1], c_std[2], c_std[3], c_std[4]],
          [0, 1, 0, 0, 0, 0],
          [0, 0, 1, 0, 0, 0],
          [0, 0, 0, 1, 0, 0],
@@ -104,7 +113,7 @@ def ARModelSimple():
     std_dev = np.sqrt(noiseVar)
     z0 = np.random.normal(0, scale=std_dev)  
     w0 = np.array([0, z0, 0, 0, 0, 0]) 
-    X = A @ init + w0
+    X = A @ init + np.transpose(w0)
     z = np.random.normal(0, scale=std_dev, size=simLength*96) #96 15 minuttes in a day 
     data = [[X[0], X[1]]]
     for i in range(len(z)):
@@ -112,6 +121,38 @@ def ARModelSimple():
         w = np.array([0, z[i], 0, 0, 0, 0])
         w = np.transpose(w)
         X = A @ X + w
+        data.append(X[:2])
+    # print(data)
+    return data
+
+def pipeTemp(n, phi):
+    return 10*np.sin(n*2*np.pi/(365*samplesDay) + phi + np.pi) + 66
+
+def groundTemp(n, phi):
+    return 8*np.sin(n*2*np.pi/(365*samplesDay) + phi) + 19
+
+def tempModel(start):
+    phi = start*np.pi/6 - 2*np.pi/3
+    A = np.array([[1, t0, 0, 0],
+         [0, c_temp[0], c_temp[1], c_temp[2]],
+         [0, 1, 0, 0],
+         [0, 0, 1, 0]])
+    B = np.array([0, k_Temp, 0, 0])
+    B = np.transpose(B)
+    std_dev = np.sqrt(noiseVarTemp)
+    z0 = np.random.normal(0, scale=std_dev)  
+    w0 = np.array([0, z0, 0, 0]) 
+    Temp0 = (pipeTemp(0, phi) + groundTemp(0, phi))/2
+
+    X = A @ init_Temp + Temp0*B + w0
+    z = np.random.normal(0, scale=std_dev, size=int(simLength*samplesDay)) #96 15 minuttes in a day 
+    data = [[X[0], X[1]]]
+    for i in range(len(z)):
+        Temp = (pipeTemp(i, phi) + groundTemp(i, phi))/2 - 25
+        # print(f"Temperature a time {i}: {Temp}, pipeTemp: {pipeTemp(i, phi)}")
+        w = np.array([0, z[i], 0, 0])
+        w = np.transpose(w)
+        X = A @ X + Temp*B + w
         data.append(X[:2])
     # print(data)
     return data
@@ -161,7 +202,11 @@ def main ():
     # plotData(data)
     
     # Plot 10 realizations on the same plot
-    plot_multiple_realizations(num_realizations=10)
+    # plot_multiple_realizations(num_realizations=10)
+
+    #Temperature based drift
+    data = tempModel(start = 1) #Write month number
+    plotData(data)
     
 
 if __name__=="__main__":
