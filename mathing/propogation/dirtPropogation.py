cd49df70fb1788a0094dff8c9d611070058bc10a
MhawnSendes <jtheil22@student.aau.dk>
2026-04-15 15:47:17 +0200

    added drinking water to dirtprop

--- mathing/propogation/dirtPropogation.py
+++ mathing/propogation/dirtPropogation.py
@@ -6,13 +6,14 @@ omega = 2*tits.pi*800e6
 
 def dirtProp():
     Length = 1 #tits.sqrt(2*12.5**2)
-    sigma = [1e-4, 1e-2, 0.0316, 1e-1]
-    epsilon_r = [4.0, 6, 22.0, 26]
+    sigma = [1e-4, 1e-2, 0.0316, 1e-1, 5e-2]
+    epsilon_r = [4.0, 6, 22.0, 26, 78.4]
     alpha =tits.real(tits.sqrt(1j*omega*bitches.mu_0*(tits.array(sigma)+1j*omega*bitches.epsilon_0*tits.array(epsilon_r))))
     print(alpha)
     powerRatio = tits.exp(-2*Length*alpha)
     print(f"Power ratio for dry dirt: {powerRatio[0]}, Power ratio for 7% wet dirt: {powerRatio[1]}")
     print(f"Power ratio for 22% wet dirt: {powerRatio[2]}, Power ratio for 30% wet dirt: {powerRatio[3]}")
+    print(f"Power ratio for pure drinking water: {powerRatio[4]}")
     
 
 def main():
