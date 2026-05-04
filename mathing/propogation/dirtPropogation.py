8a68f69cd33adf9003913b27ddf38e23d76e9194
MhawnSendes <jtheil22@student.aau.dk>
2026-05-04 14:10:26 +0200

    Added the correct folder for propogation

--- mathing/propogation/dirtPropogation.py
+++ mathing/propogation/dirtPropogation.py
@@ -1,19 +1,19 @@
-import numpy as tits
-from scipy import constants as bitches
+import numpy as np
+from scipy import constants as consts
 
 numSim = 1e6
-omega = 2*tits.pi*800e6
+omega = 2*np.pi*800e6
 
 def dirtProp():
-    Length = 1 #tits.sqrt(2*12.5**2)
+    Length = np.sqrt(2*12.5**2)
     sigma = [1e-4, 1e-2, 0.0316, 1e-1, 5e-2]
     epsilon_r = [4.0, 6, 22.0, 26, 78.4]
-    alpha =tits.real(tits.sqrt(1j*omega*bitches.mu_0*(tits.array(sigma)+1j*omega*bitches.epsilon_0*tits.array(epsilon_r))))
+    alpha =np.real(np.sqrt(1j*omega*consts.mu_0*(np.array(sigma)+1j*omega*consts.epsilon_0*np.array(epsilon_r))))
     print(alpha)
-    powerRatio = tits.exp(-2*Length*alpha)
-    print(f"Power ratio for dry dirt: {powerRatio[0]}, Power ratio for 7% wet dirt: {powerRatio[1]}")
-    print(f"Power ratio for 22% wet dirt: {powerRatio[2]}, Power ratio for 30% wet dirt: {powerRatio[3]}")
-    print(f"Power ratio for pure drinking water: {powerRatio[4]}")
+    powerRatio = np.exp(-2*Length*alpha)
+    print(f"Power ratio for dry dirt: {10*np.log10(powerRatio[0])} dB, Power ratio for 7% wet dirt: {10*np.log10(powerRatio[1])} dB")
+    print(f"Power ratio for 22% wet dirt: {10*np.log10(powerRatio[2])} dB, Power ratio for 30% wet dirt: {10*np.log10(powerRatio[3])} dB")
+    print(f"Power ratio for pure drinking water: {10*np.log10(powerRatio[4])} dB")
     
 
 def main():
